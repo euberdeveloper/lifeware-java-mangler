cf393764bd73776d8d61267e3388cfe1eaa8c9ce
euberdeveloper <euberdeveloper@gmail.com>
2022-07-19 14:40:02 +0200

    test: add test to methods
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- test/suites/mangleMethodOrConstructor.test.ts
+++ test/suites/mangleMethodOrConstructor.test.ts
@@ -5,51 +5,60 @@ import { mangleConstructor, mangleMethod } from '@src/modules/mangleMethodOrCons
 export default function testMangleMethodOrConstructor(): void {
     describe('Test @/modules/mangleMethodOrConstructor', function () {
         describe('Methods', function () {
-            it(`Should mangle "void print();"`, function () {
-                const expected = 'j_m_Vprint_C';
-                const result = mangleMethod('void print();');
+            describe('Without parameters', function () {
+                it(`Should mangle "void print();"`, function () {
+                    const expected = 'j_m_Vprint_C';
+                    const result = mangleMethod('void print();');
 
-                expect(result).to.equal(expected);
-            });
+                    expect(result).to.equal(expected);
+                });
 
-            it(`Should mangle "     void     print(    )  ;      "`, function () {
-                const expected = 'j_m_Vprint_C';
-                const result = mangleMethod('     void     print(    )  ;      ');
+                it(`Should mangle "     void     print(    )  ;      "`, function () {
+                    const expected = 'j_m_Vprint_C';
+                    const result = mangleMethod('     void     print(    )  ;      ');
 
-                expect(result).to.equal(expected);
-            });
+                    expect(result).to.equal(expected);
+                });
 
-            it(`Should mangle "boolean isEmpty();"`, function () {
-                const expected = 'j_m_ZisEmpty_C';
-                const result = mangleMethod('boolean isEmpty();');
+                it(`Should mangle "boolean isEmpty();"`, function () {
+                    const expected = 'j_m_ZisEmpty_C';
+                    const result = mangleMethod('boolean isEmpty();');
 
-                expect(result).to.equal(expected);
-            });
+                    expect(result).to.equal(expected);
+                });
 
-            it(`Should mangle " boolean isEmpty(         )"`, function () {
-                const expected = 'j_m_ZisEmpty_C';
-                const result = mangleMethod(' boolean isEmpty(         )');
-                expect(result).to.equal(expected);
-            });
+                it(`Should mangle " boolean isEmpty(         )"`, function () {
+                    const expected = 'j_m_ZisEmpty_C';
+                    const result = mangleMethod(' boolean isEmpty(         )');
+                    expect(result).to.equal(expected);
+                });
 
-            it(`Should mangle "int count()"`, function () {
-                const expected = 'j_m_Icount_C';
-                const result = mangleMethod('int count()');
+                it(`Should mangle "int count()"`, function () {
+                    const expected = 'j_m_Icount_C';
+                    const result = mangleMethod('int count()');
 
-                expect(result).to.equal(expected);
-            });
+                    expect(result).to.equal(expected);
+                });
 
-            it(`Should mangle " int count(         )"   `, function () {
-                const expected = 'j_m_Icount_C';
-                const result = mangleMethod(' int count(         )');
-                expect(result).to.equal(expected);
-            });
+                it(`Should mangle " int count(         )"   `, function () {
+                    const expected = 'j_m_Icount_C';
+                    const result = mangleMethod(' int count(         )');
+                    expect(result).to.equal(expected);
+                });
 
-            it(`Should mangle " java.lang.String getName ();"`, function () {
-                const expected = 'j_m_Ljava_Slang_SString_MgetName_C';
-                const result = mangleMethod(' java.lang.String getName ();');
+                it(`Should mangle " java.lang.String getName ();"`, function () {
+                    const expected = 'j_m_Ljava_Slang_SString_MgetName_C';
+                    const result = mangleMethod(' java.lang.String getName ();');
 
-                expect(result).to.equal(expected);
+                    expect(result).to.equal(expected);
+                });
+
+                it(`Should mangle " int _underscoredAndNumb3red ();"`, function () {
+                    const expected = 'j_m_I__underscoredAndNumb3red_C';
+                    const result = mangleMethod(' int _underscoredAndNumb3red ();');
+
+                    expect(result).to.equal(expected);
+                });
             });
         });
 
