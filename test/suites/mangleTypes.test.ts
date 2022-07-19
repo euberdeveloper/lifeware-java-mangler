e3a6e773f14c77f3d697d42f4bab9d30b96f23fe
euberdeveloper <euberdeveloper@gmail.com>
2022-07-19 14:17:51 +0200

    test: add test for matrices
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- test/suites/mangleTypes.test.ts
+++ test/suites/mangleTypes.test.ts
@@ -106,66 +106,135 @@ export default function testMangleTypes(): void {
         });
 
         describe('Array types', function () {
-            it(`Should mangle correctly an array of boolean`, function () {
-                const expected = '_BZ';
-                const result = mangleType('boolean[]');
+            describe('Primitive types', function () {
+                it(`Should mangle correctly an array of boolean`, function () {
+                    const expected = '_BZ';
+                    const result = mangleType('boolean[]');
 
-                expect(result).to.equal(expected);
+                    expect(result).to.equal(expected);
+                });
+
+                it(`Should mangle correctly an array of byte`, function () {
+                    const expected = '_BB';
+                    const result = mangleType('byte[]');
+
+                    expect(result).to.equal(expected);
+                });
+
+                it(`Should mangle correctly an array of char`, function () {
+                    const expected = '_BC';
+                    const result = mangleType('char[]');
+
+                    expect(result).to.equal(expected);
+                });
+                it(`Should mangle correctly an array of double`, function () {
+                    const expected = '_BD';
+                    const result = mangleType('double[]');
+
+                    expect(result).to.equal(expected);
+                });
+
+                it(`Should mangle correctly an array of float`, function () {
+                    const expected = '_BF';
+                    const result = mangleType('float[]');
+
+                    expect(result).to.equal(expected);
+                });
+
+                it(`Should mangle correctly an array of int`, function () {
+                    const expected = '_BI';
+                    const result = mangleType('int[]');
+
+                    expect(result).to.equal(expected);
+                });
+
+                it(`Should mangle correctly an array of long`, function () {
+                    const expected = '_BJ';
+                    const result = mangleType('long[]');
+
+                    expect(result).to.equal(expected);
+                });
+
+                it(`Should mangle correctly an array of short`, function () {
+                    const expected = '_BS';
+                    const result = mangleType('short[]');
+
+                    expect(result).to.equal(expected);
+                });
+
+                it(`Should mangle correctly an array of void`, function () {
+                    const expected = '_BV';
+                    const result = mangleType('void[]');
+
+                    expect(result).to.equal(expected);
+                });
             });
 
-            it(`Should mangle correctly an array of byte`, function () {
-                const expected = '_BB';
-                const result = mangleType('byte[]');
+            describe('Object types', function () {
+                it(`Should mangle correctly an array of objects without package "String"`, function () {
+                    const expected = '_BLString_M';
+                    const result = mangleType('String[]');
 
-                expect(result).to.equal(expected);
+                    expect(result).to.equal(expected);
+                });
+
+                it(`Should mangle correctly an array of objects without package "BurunduBurundu"`, function () {
+                    const expected = '_BLBurunduBurundu_M';
+                    const result = mangleType('BurunduBurundu[]');
+
+                    expect(result).to.equal(expected);
+                });
+
+                it(`Should mangle correctly an array of objects with package "java.lang.String"`, function () {
+                    const expected = '_BLjava_Slang_SString_M';
+                    const result = mangleType('java.lang.String[]');
+
+                    expect(result).to.equal(expected);
+                });
+
+                it(`Should mangle correctly an array of objects with package "java.burundi.Burundu"`, function () {
+                    const expected = '_BLjava_Sburundi_SBurundu_M';
+                    const result = mangleType('java.burundi.Burundu[]');
+
+                    expect(result).to.equal(expected);
+                });
+
+                it(`Should mangle correctly an array of objects with package and nested class "java.burundi.Burundu.Gabibbo"`, function () {
+                    const expected = '_BLjava_Sburundi_SBurundu_DGabibbo_M';
+                    const result = mangleType('java.burundi.Burundu.Gabibbo[]');
+
+                    expect(result).to.equal(expected);
+                });
             });
 
-            it(`Should mangle correctly an array of char`, function () {
-                const expected = '_BC';
-                const result = mangleType('char[]');
+            describe('Multidimension arrays', function () {
+                it(`Should mangle correctly a matrices of bytes`, function () {
+                    const expected = '_B_BB';
+                    const result = mangleType('byte[][]');
 
-                expect(result).to.equal(expected);
-            });
-            it(`Should mangle correctly an array of double`, function () {
-                const expected = '_BD';
-                const result = mangleType('double[]');
+                    expect(result).to.equal(expected);
+                });
 
-                expect(result).to.equal(expected);
-            });
+                it(`Should mangle correctly a cube of bytes`, function () {
+                    const expected = '_B_B_BB';
+                    const result = mangleType('byte[][][]');
 
-            it(`Should mangle correctly an array of float`, function () {
-                const expected = '_BF';
-                const result = mangleType('float[]');
+                    expect(result).to.equal(expected);
+                });
 
-                expect(result).to.equal(expected);
-            });
+                it(`Should mangle correctly a 5x5 of bytes`, function () {
+                    const expected = '_B_B_B_B_BB';
+                    const result = mangleType('byte[][][][][]');
 
-            it(`Should mangle correctly an array of int`, function () {
-                const expected = '_BI';
-                const result = mangleType('int[]');
+                    expect(result).to.equal(expected);
+                });
 
-                expect(result).to.equal(expected);
-            });
+                it(`Should mangle correctly a 5x5 of Strings`, function () {
+                    const expected = '_B_B_B_B_BLjava_Slang_SString_M';
+                    const result = mangleType('java.lang.String[][][][][]');
 
-            it(`Should mangle correctly an array of long`, function () {
-                const expected = '_BJ';
-                const result = mangleType('long[]');
-
-                expect(result).to.equal(expected);
-            });
-
-            it(`Should mangle correctly an array of short`, function () {
-                const expected = '_BS';
-                const result = mangleType('short[]');
-
-                expect(result).to.equal(expected);
-            });
-
-            it(`Should mangle correctly an array of void`, function () {
-                const expected = '_BV';
-                const result = mangleType('void[]');
-
-                expect(result).to.equal(expected);
+                    expect(result).to.equal(expected);
+                });
             });
         });
     });
