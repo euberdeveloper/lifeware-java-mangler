826f1c625dc56fef9e5b00168f7d15a6a7eb106f
euberdeveloper <euberdeveloper@gmail.com>
2023-02-07 12:47:12 +0100

    build: fix building
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- build.mjs
+++ build.mjs
@@ -1,32 +1,46 @@
+import fs from 'node:fs';
 import { build } from 'esbuild';
 import { generateDtsBundle } from 'dts-bundle-generator';
 
 import packageJson from './package.json' assert { type: 'json' };
 
-generateDtsBundle([
-    {
-        filePath: './source/index.ts',
-        output: './bundled/index.d.ts'
-    },
-    
-]);
 
-const shared = {
-    entryPoints: ['source/index.ts'],
-    bundle: true,
-    minify: true,
-    treeShaking: true,
-    external: packageJson.dependencies ?? []
-};
+async function buildModule() {
+    const shared = {
+        entryPoints: ['source/index.ts'],
+        bundle: true,
+        minify: true,
+        treeShaking: true,
+        external: packageJson.dependencies ?? []
+    };
 
-build({
-    ...shared,
-    outfile: 'bundled/index.js',
-    format: 'cjs'
-});
+    await build({
+        ...shared,
+        outfile: 'bundled/commonjs/index.js',
+        format: 'cjs'
+    });
 
-build({
-    ...shared,
-    outfile: 'bundled/index.esm.js',
-    format: 'esm',
-});
+    await build({
+        ...shared,
+        outfile: 'bundled/esm/index.esm.js',
+        format: 'esm',
+    });
+}
+
+function buildDts() {
+    generateDtsBundle([
+        {
+            filePath: './source/index.ts',
+            output: './bundled/index.d.ts'
+        },
+    ]);
+}
+
+function generateCommonjsPackageJson() {
+    const packageJsonCommonJs = JSON.stringify({ ...packageJson, type: undefined }, null, 2);
+    fs.writeFileSync('./bundled/commonjs/package.json', packageJsonCommonJs);
+}
+
+await buildModule();
+buildDts();
+generateCommonjsPackageJson();
